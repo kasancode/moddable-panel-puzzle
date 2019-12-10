53d7af0ffdcf9c8f7a97545e2c2217f8e3bb23a7
kasancode <kasancode@gmail.com>
2019-12-11 02:05:06 +0900

    fix

--- main.ts
+++ main.ts
@@ -17,16 +17,20 @@ const panelSkin = new Skin({
     fill: ["#ff0000", "#0000ff", "#ff1493", "#4169e1"]
 });
 
-let panels: boolean[][] = Array.apply(null, Array(size))
-    .map(() => Array.apply(null, Array(size))
-        .map(() => Math.random() < 0.5));
+function range(n:number):any[]{
+    return Array.apply(null, Array(n));
+}
+
+let panels: boolean[][] = range(size)
+    .map(() => range(size)
+        .map(() => false));
 
 function getState(lx: number, ly: number): number {
     return (panels[lx][ly] ? 1 : 0) + ((lx === x && ly === y) ? 2 : 0);
 }
 
 function createContents(): Content[] {
-    return Array.apply(null, Array(size * size))
+    return range(size * size)
         .map((_, i: number) => {
             let x = i % size;
             let y = Math.floor(i / size);
@@ -69,7 +73,7 @@ function toggleOne(x: number, y: number): void {
     panels[x][y] = !panels[x][y];
 }
 
-function render() {
+function render(): void {
     for (let x = 0; x < size; x++)
         for (let y = 0; y < size; y++)
             PanelsApplication.content(String(x + y * size)).state = getState(x, y);
@@ -107,5 +111,19 @@ buttonC.onChanged = function () {
     if (this.read() === 0)
         toggle();
 }
+
+for (let i = 0; i < 100; i++) {
+    x += Math.random() * size;
+    y += Math.random() * size;
+
+    x = Math.floor(x) % size;
+    y = Math.floor(y) % size;
+
+    toggle();
+}
+
+x = 0;
+y = 0;
+render();
 
 export default PanelsApplication;
